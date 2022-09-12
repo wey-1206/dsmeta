dbf387bb983a515313ee147c97fa1ff29076971e
Wesley Prado <prado@MacBook-Pro-de-Prado.local>
2022-09-12 11:16:03 -0300

    DataBase H2

--- backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -2,8 +2,18 @@ package com.devsuperior.dsmeta.entities;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sales")
 public class Sale {
 	 
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String sellerName;
 	private Integer visited;
